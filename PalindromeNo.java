0bd14455c089214f6deec5757b881734842fde22
Nal2001 <53413351+Nal2001@users.noreply.github.com>
2022-07-30 02:18:26 +0530

    Update PalindromeNo.java

--- PalindromeNo.java
+++ PalindromeNo.java
@@ -1,25 +1,14 @@
-import java.util.Scanner;
-
-public class PalindromeNo {
-    public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
-        System.out.println("Enter  a number:");
-        int num=input.nextInt();
+import java.util.*;
+class Solution {
+    public boolean isPalindrome(int x) {
         int remainder,sum=0;
-        int temp=num;
-        while(num>0){
-            remainder=num%10;
+        int temp=x;
+        while(x>0){
+            remainder=x%10;
            sum=(sum*10)+remainder;
-           num=num/10;
+           x=x/10;
+        }
+    return sum==temp;
+        
     }
-      
-        if(temp==sum){
-            System.out.println("The number is palindrome:");
-        }
-        else{
-            System.out.println("The number is not palindrome:");
-        }
-    
-    
-}
 }
